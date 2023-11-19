f81408c563d47da6cdb3656454392cdd52de05e5
lmm21 <sa84744@sa3.so-net.ne.jp>
2023-11-19 20:04:12 +0900

    STEP9 - セミコロン区切りで複数の文（ステートメント）を書けるように. test-step-9.sh - テストコード追加、デバッグオプション追加.

---
+++ step-9-Accept_multiple_statements_separated_by_semicolons/test-chibicc-step-9.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# DEBUGモード
+if [ "$#" = "1" ] && [ "$1" = "-d" ]; then
+    CPPFLAGS="-DDEBUG";
+    LDFLAGS="-static"
+    gcc $LDFLAGS $CPPFLAGS -o chibicc main.c parse.c codegen.c tokenize.c;
+    ./chibicc "1 + 1;1 + 1; 1 + 1;" > test.s;
+    cat test.s;
+    exit 0;
+fi
+
+assert() {
+  expected="$1"
+  input="$2"
+
+  ./chibicc "$input" > tmp.s
+  gcc -static -o tmp tmp.s
+  ./tmp
+  actual="$?"
+
+  if [ "$actual" = "$expected" ]; then
+    echo "$input => $actual"
+  else
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+assert 0 '0;'
+assert 42 '42;'
+assert 21 '5+20-4;'
+assert 41 ' 12 + 34 - 5 ;'
+assert 47 '5+6*7;'
+assert 15 '5*(9-6);'
+assert 4 '(3+5)/2;'
+assert 10 '-10+20;'
+assert 10 '- -10;'
+assert 10 '- - +10;'
+
+assert 0 '0==1;'
+assert 1 '42==42;'
+assert 1 '0!=1;'
+assert 0 '42!=42;'
+
+assert 1 '0<1;'
+assert 0 '1<1;'
+assert 0 '2<1;'
+assert 1 '0<=1;'
+assert 1 '1<=1;'
+assert 0 '2<=1;'
+
+assert 1 '1>0;'
+assert 0 '1>1;'
+assert 0 '1>2;'
+assert 1 '1>=0;'
+assert 1 '1>=1;'
+assert 0 '1>=2;'
+
+assert 3 '1; 2; 3;'
+
+echo OK
